978ee2a741128eb40069389f1687feb93083dd6e
albetorres <albertocorreoficial@gmail.com>
2022-12-01 17:47:07 -0500

    agregado responsive

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -8,41 +8,44 @@ import social3 from "../assets/img/nav-icon3.svg";
 
 const Hero = () => {
   return (
-    <div className="grid gap-x-4 herolayout py-32  ">
-      <div>
+    <div className="grid gap-y-5 md:gap-x-4 herolayout py-32  ">
+      <div className=" flex flex-col items-center md:items-start">
         <div className="p-4 rounded-md border w-fit banner tagline ">
           <p>Welcome to my portfolio</p>
         </div>
-        <h1 className="font-sans text-7xl mb-4 font-semibold">
+        <h1 className="font-sans text-4xl text-center md:text-start md:text-7xl mb-4 font-semibold">
           Hi! i'm Albert a Web Developer
         </h1>
-        <p className="">
+        <p className=" text-center md:text-start">
           I am a programmer motivated by the idea of creating solutions that add
           value to society. I consider myself an empathetic, responsible person
           with the ability to transmit ideas. I am always learning something
           new, it is a way of life that I enjoy. I am interested in working with
           talented colleagues in a healthy environment.
         </p>
-        <ul className="flex items-center justify-evenly w-1/4 my-4  ">
-          <li className="flex items-center rounded-full border ">
-            <a href="   " className=" cursor-pointer inline-block  p-2 rounded-full">
+        <ul className="flex items-center justify-between w-40     my-5  ">
+          <li className="social-icon ">
+            <a href="   " className=" md:mr-2  ">
               <img src={social1} alt="" />
             </a>
           </li>
-          <li className=" flex items-center rounded-full border ">
-            <a href="" className="w-full h-full cursor-pointer inline-block p-2 rounded-full">
+          <li className=" social-icon ">
+            <a href="" className="md:mr-2 ">
               <img src={social2} alt="" />
             </a>
           </li>
-          <li className="flex items-center rounded-full border ">
-            <a href="" className="w-full h-full cursor-pointer inline-block p-2 rounded-full">
-              <img src={social3} alt="" />
+          <li className="social-icon  ">
+            <a href="" className=" md:mr-2   ">
+              <img src={social3}  alt="" />
             </a>
           </li>
         </ul>
         <div className="font-sans ">
             <button className="conectbtn py-3 px-4 border rounded-md ">
+                <span>
+
                 Let's Connect
+                </span> 
             </button>
         </div>
       </div>
